d0fe78fd08b81cb6025d42d0b53c16650f84b7fb
Sergei Mikurov <454004@mail.ru>
2025-05-14 02:33:13 +0500

    chore(utils): Add logger

---
+++ src/wse_exercises/utils/logger.py
@@ -0,0 +1,25 @@
+"""Logging system initialization."""
+
+import logging
+import sys
+
+
+def setup_logging(name: str, log_file: str | None = None) -> logging.Logger:
+    """Initialize logging system."""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
