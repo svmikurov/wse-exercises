5d46d6629f55b70d73bf503e1d10ffaa050aa4a1
Sergei Mikurov <454004@mail.ru>
2025-05-23 03:09:37 +0500

    perf(mathem): Add imports with `__all__`

--- src/wse_exercises/__init__.py
+++ src/wse_exercises/__init__.py
@@ -1,1 +1,21 @@
 """WSE Exercise Series."""
+
+__all__ = [
+    'AddingExercise',
+    'DivisionExercise',
+    'ExactOperandGenerator',
+    'MultiplicationExercise',
+    'RandomOperandGenerator',
+    'SimpleMathExerciseConfig',
+    'SubtractionExercise',
+]
+
+from wse_exercises.core.mathem import (
+    AddingExercise,
+    DivisionExercise,
+    ExactOperandGenerator,
+    MultiplicationExercise,
+    RandomOperandGenerator,
+    SimpleMathExerciseConfig,
+    SubtractionExercise,
+)
