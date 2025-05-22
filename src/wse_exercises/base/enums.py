27e492f60093e81c7152ff6d0e4eb3d19ca7935d
Sergei Mikurov <454004@mail.ru>
2025-05-23 03:07:49 +0500

    style(base): Add `value` annotaons with `str`

--- src/wse_exercises/base/enums.py
+++ src/wse_exercises/base/enums.py
@@ -6,6 +6,8 @@ from enum import Enum
 class BaseEnum(str, Enum):
     """Base class for enumerations."""
 
+    value: str
+
     def __str__(self) -> str:
         """Return button text."""
         return self.value
