03e656997ab4d4242f98bda3198b97ecf2d778e8
Sergei Mikurov <454004@mail.ru>
2025-06-30 22:24:41 +0500

    feat(base/task): Add methods

--- src/wse_exercises/base/task.py
+++ src/wse_exercises/base/task.py
@@ -1,13 +1,16 @@
 """Defines base class for task."""
 
 from datetime import datetime
-from typing import Generic
+from typing import Any, Generic, Type, TypeVar
 
 from pydantic import BaseModel, Field
 
+# TODO: Fix import
 from wse_exercises.core.mathem.enums import Exercises
 
-from .interface import AnswerT, QuestionT, TaskConditionsT, TaskConfigT
+from .interfaces import AnswerT, QuestionT, TaskConditionsT, TaskConfigT
+
+T = TypeVar('T', bound='Task[Any, Any, Any, Any]')
 
 
 class TaskConfig(BaseModel):
@@ -27,3 +30,35 @@ class Task(
     exercise_name: Exercises
     created: datetime = Field(default_factory=datetime.now)
     error_msg: str = Field(default='', frozen=True)
+
+    @classmethod
+    def from_dict(cls: Type[T], data: dict[str, Any]) -> T:
+        """Instantiate the class from a dictionary of attributes."""
+        return cls.parse_obj(data)
+
+    @classmethod
+    def from_json(cls: Type[T], data: str | bytes) -> T:
+        """Instantiate the class from a JSON string or bytes."""
+        return cls.parse_raw(data)
+
+    def to_dict(self) -> dict[str, Any]:
+        """Convert the instance to a dictionary."""
+
+        def convert_datetime(data: dict[str, Any]) -> dict[str, Any]:
+            dict_data: dict[str, Any] = {}
+
+            for key, value in data.items():
+                if isinstance(value, datetime):
+                    dict_data[key] = value.isoformat()
+                elif isinstance(value, dict):
+                    dict_data[key] = convert_datetime(value)
+                else:
+                    dict_data[key] = value
+
+            return dict_data
+
+        return convert_datetime(self.dict())
+
+    def to_json(self) -> str:
+        """Serialize the instance to a JSON string."""
+        return self.json()
