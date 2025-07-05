eecc1e78e4966e6eb9e07a835c2bef079528a29c
Sergei Mikurov <454004@mail.ru>
2025-07-05 13:44:58 +0500

    feat(core): Add exercise type mapping
    - Added `EXERCISES` the mapping exercise name with exercise type

--- src/wse_exercises/core/__init__.py
+++ src/wse_exercises/core/__init__.py
@@ -2,6 +2,20 @@
 
 __all__ = [
     'Exercises',
+    'EXERCISES',
 ]
 
+from wse_exercises.core.math import (
+    AddingExercise,
+    DivisionExercise,
+    MultiplicationExercise,
+    SubtractionExercise,
+)
 from wse_exercises.core.math.enums import Exercises
+
+EXERCISES = {
+    Exercises.ADDING: AddingExercise,
+    Exercises.SUBTRACTION: SubtractionExercise,
+    Exercises.MULTIPLICATION: MultiplicationExercise,
+    Exercises.DIVISION: DivisionExercise,
+}
